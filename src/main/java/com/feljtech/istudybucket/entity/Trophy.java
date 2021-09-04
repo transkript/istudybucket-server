c315ec71d1c5c824d40387289a01e9232e12e9c2
andrew21-mch <nfonandrew73@gmail.com>
2021-09-04 18:38:14 +0100

    added Media class

---
+++ src/main/java/com/feljtech/istudybucket/entity/Trophy.java
@@ -0,0 +1,16 @@
+package com.feljtech.istudybucket.entity;
+
+public class Trophy {
+    private Long id;
+
+    private Long userId;
+
+    private Long commentId;
+
+    private String TrophyType;
+
+    private String trophyName;
+
+    private String trophyImageUrl;
+
+}
