e5d58f91614027efb8e97148dc2d52631ae925c0
Elroy Kanye <elroykanye@gmail.com>
2021-09-13 03:43:46 +0100

    ek-autocomm: added ./src/main/java/com/feljtech/istudybucket/config/SecurityConfig.java

---
+++ src/main/java/com/feljtech/istudybucket/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.feljtech.istudybucket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/api/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
