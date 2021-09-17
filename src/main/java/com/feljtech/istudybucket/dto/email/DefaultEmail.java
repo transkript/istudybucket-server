e30d7251edef8e9db1c5241deade0199642b906e
elroykanye <elroykanye@gmail.com>
2021-09-17 17:35:06 +0100

    mail sending service impl: modified ./src/main/java/com/feljtech/istudybucket/dto/email/DefaultEmail.java

--- src/main/java/com/feljtech/istudybucket/dto/email/DefaultEmail.java
+++ src/main/java/com/feljtech/istudybucket/dto/email/DefaultEmail.java
@@ -1,2 +1,16 @@
-package com.feljtech.istudybucket.dto.email;public class Email {
+package com.feljtech.istudybucket.dto.email;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class DefaultEmail {
+    private String recipient;
+    private String subject;
+    private String message;
 }
