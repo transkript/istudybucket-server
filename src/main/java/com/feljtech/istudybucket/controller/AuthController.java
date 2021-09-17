02f7111c2a4dced56ca7f94ab89940ecd91d2d3c
elroykanye <elroykanye@gmail.com>
2021-09-17 19:34:16 +0100

    authentication by uuid impl: modified ./src/main/java/com/feljtech/istudybucket/controller/AuthController.java

--- src/main/java/com/feljtech/istudybucket/controller/AuthController.java
+++ src/main/java/com/feljtech/istudybucket/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.feljtech.istudybucket.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -28,9 +25,22 @@ public class AuthController {
 
     private final AuthService authService;
 
+    /**
+     * registers a user from a RegisterForm object, consumed from JSON.
+     * typically, control is passed to the userService bean.
+     * @param registerForm: the form as an object - spring auto converts based on input names.
+     * @return http status of the process
+     */
     @PostMapping(value = "/register")
-    public ResponseEntity<String> register(@RequestBody RegisterForm registerForm, HttpServletRequest request) {
-        authService.register(registerForm);
+    public ResponseEntity<String> register(@RequestBody RegisterForm registerForm) {
+        authService.registerAccount(registerForm);
         return new ResponseEntity<>("User registration successful", HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/verify/{verToken}")
+    public ResponseEntity<String> verify(@PathVariable(value = "verToken") String verificationToken, String username) {
+        return authService.verifyAccount(verificationToken, username) ?
+                new ResponseEntity<>("User verification successful", HttpStatus.ACCEPTED):
+                new ResponseEntity<>("User verification unsuccessful", HttpStatus.BAD_REQUEST);
     }
 }
