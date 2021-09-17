dd01ba18682038f1753e361783f59befb008259b
elroykanye <elroykanye@gmail.com>
2021-09-17 17:35:04 +0100

    mail sending service impl: modified ./src/main/java/com/feljtech/istudybucket/service/impl/MailServiceImpl.java

--- src/main/java/com/feljtech/istudybucket/service/impl/MailServiceImpl.java
+++ src/main/java/com/feljtech/istudybucket/service/impl/MailServiceImpl.java
@@ -1,10 +1,10 @@
 package com.feljtech.istudybucket.service.impl;
 
-import com.feljtech.istudybucket.dto.email.NotificationEmail;
+import com.feljtech.istudybucket.dto.email.VerificationEmail;
+import com.feljtech.istudybucket.enums.Email;
 import com.feljtech.istudybucket.excetion.IstudybucketException;
+import com.feljtech.istudybucket.service.MailContentBuilder;
 import com.feljtech.istudybucket.service.MailService;
-import freemarker.template.Configuration;
-import freemarker.template.TemplateException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.MailException;
@@ -13,42 +13,34 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
-
 @Service
 @AllArgsConstructor
 @Slf4j
 public class MailServiceImpl implements MailService {
-    private final Configuration configuration;
     private final JavaMailSender javaMailSender;
+    private final MailContentBuilder mailContentBuilder;
 
 
-    public void sendEmail(NotificationEmail notificationEmail) {
+    @Override
+    public void sendVerificationEmail(VerificationEmail verificationEmail) {
+        MimeMessagePreparator mimeMessagePreparator = mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom(String.valueOf(Email.DEFAULT_FROM)); // set the email from to the default application from address
+            mimeMessageHelper.setSubject(verificationEmail.getSubject()); // set the email subject
+            mimeMessageHelper.setTo(verificationEmail.getRecipient()); // set the email recipient
 
-        MimeMessagePreparator mimeMessagePreparator = message -> {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message);
-            mimeMessageHelper.setFrom("elroykanye@gmail.com");
-            mimeMessageHelper.setSubject(notificationEmail.getSubject());
-            mimeMessageHelper.setTo(notificationEmail.getRecipient());
-            String emailContent = this.buildEmail(notificationEmail.getBody());
+            String emailContent = mailContentBuilder.buildVerificationEmailContent(verificationEmail);
             mimeMessageHelper.setText(emailContent, true);
         };
         try {
             javaMailSender.send(mimeMessagePreparator);
-            log.info("Activation Email Sent!");
-        } catch(MailException exception) {
-            throw new IstudybucketException("Activation Email not sent");
+            log.info("Verification email sent");
+        } catch(MailException mailException) {
+            log.error("Verification email not sent");
+            throw new IstudybucketException("Could not send email");
         }
+
+
     }
 
-    public String buildEmail(String message) throws IOException, TemplateException {
-        Map<String, String> messageModel = new HashMap<>();
-        StringWriter stringWriter = new StringWriter();
-        messageModel.put("message", message);
-        configuration.getTemplate("mail.ftl").process(messageModel, stringWriter);
-        return stringWriter.getBuffer().toString();
-    }
 }
