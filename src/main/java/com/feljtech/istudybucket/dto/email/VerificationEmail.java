59dec3d5d8fd0f07af63b909c6464637bf2a7e47
elroykanye <elroykanye@gmail.com>
2021-09-17 17:35:06 +0100

    mail sending service impl: modified ./src/main/java/com/feljtech/istudybucket/dto/email/VerificationEmail.java

--- src/main/java/com/feljtech/istudybucket/dto/email/VerificationEmail.java
+++ src/main/java/com/feljtech/istudybucket/dto/email/VerificationEmail.java
@@ -1,2 +1,14 @@
-package com.feljtech.istudybucket.dto.email;public class VerificationEmail {
+package com.feljtech.istudybucket.dto.email;
+
+
+import lombok.*;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class VerificationEmail extends DefaultEmail {
+    private String verificationUrl;
+    private String recipientName;
 }
