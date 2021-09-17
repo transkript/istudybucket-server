e5becaa0711854702e1aa40400d609cd00ed2dee
elroykanye <elroykanye@gmail.com>
2021-09-17 17:35:04 +0100

    mail sending service impl: modified ./src/main/java/com/feljtech/istudybucket/service/AuthService.java

--- src/main/java/com/feljtech/istudybucket/service/AuthService.java
+++ src/main/java/com/feljtech/istudybucket/service/AuthService.java
@@ -1,10 +1,7 @@
 package com.feljtech.istudybucket.service;
 
 import com.feljtech.istudybucket.dto.form.RegisterForm;
-import org.springframework.http.HttpRequest;
-
-import javax.servlet.http.HttpServletRequest;
 
 public interface AuthService {
-    void register(RegisterForm registerForm, HttpServletRequest request);
+    void register(RegisterForm registerForm);
 }
