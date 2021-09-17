6277f4c87e3d9f27d7f8d1a2e6b2626954c6c46d
elroykanye <elroykanye@gmail.com>
2021-09-17 13:20:49 +0100

    ek-autocomm: added ./src/main/java/com/feljtech/istudybucket/dto/form/RegisterForm.java

--- src/main/java/com/feljtech/istudybucket/dto/form/RegisterForm.java
+++ src/main/java/com/feljtech/istudybucket/dto/form/RegisterForm.java
@@ -1,17 +1,20 @@
-package com.feljtech.istudybucket.form;
+package com.feljtech.istudybucket.dto.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterForm {
+    @JsonProperty("username")
     private String username;
-    private String firstName;
-    private String lastName;
-
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("password")
+    private String password;
 }
