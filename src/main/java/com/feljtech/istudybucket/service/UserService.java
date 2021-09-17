b0915542322cb82a53d8daf6f4352ab9b542a440
elroykanye <elroykanye@gmail.com>
2021-09-17 13:20:49 +0100

    ek-autocomm: added ./src/main/java/com/feljtech/istudybucket/service/UserService.java

--- src/main/java/com/feljtech/istudybucket/service/UserService.java
+++ src/main/java/com/feljtech/istudybucket/service/UserService.java
@@ -1,6 +1,6 @@
 package com.feljtech.istudybucket.service;
 
-import com.feljtech.istudybucket.form.RegisterForm;
+import com.feljtech.istudybucket.dto.form.RegisterForm;
 
 /**
  * UserService: interface for all User business logic
