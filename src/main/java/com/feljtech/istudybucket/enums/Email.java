ad39c18d16d8c35bb47311677e4ae2d0bc67903e
elroykanye <elroykanye@gmail.com>
2021-09-17 17:35:03 +0100

    mail sending service impl: modified ./src/main/java/com/feljtech/istudybucket/enums/Email.java

--- src/main/java/com/feljtech/istudybucket/enums/Email.java
+++ src/main/java/com/feljtech/istudybucket/enums/Email.java
@@ -1,2 +1,9 @@
-package com.feljtech.istudybucket.enums;public enum Email {
+package com.feljtech.istudybucket.enums;
+
+public enum Email {
+    DEFAULT_FROM("elroykanye@gmail.com"),
+    DEFAULT_SUBJECT("iStudyBucket");
+
+    Email(String s) {
+    }
 }
