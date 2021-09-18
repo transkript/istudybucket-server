ee4c97b68beea5ce7e5c8f6364965a1c6259777e
elroykanye <elroykanye@gmail.com>
2021-09-18 18:02:15 +0100

    added jwt auth using jwt api: modified ./src/main/java/com/feljtech/istudybucket/controller/AuthController.java

--- src/main/java/com/feljtech/istudybucket/controller/AuthController.java
+++ src/main/java/com/feljtech/istudybucket/controller/AuthController.java
@@ -1,13 +1,12 @@
 package com.feljtech.istudybucket.controller;
 
+import com.feljtech.istudybucket.dto.form.LoginForm;
 import com.feljtech.istudybucket.dto.form.RegisterForm;
 import com.feljtech.istudybucket.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Elroy Kanye
@@ -37,10 +36,21 @@ public class AuthController {
         return new ResponseEntity<>("User registration successful", HttpStatus.OK);
     }
 
+    /**
+     * Activates the user verification process using the sent token via email
+     * @param verificationToken: the string token received by the user
+     * @param username: username of the associated user
+     * @return response entity object describing success of operation
+     */
     @GetMapping(value = "/verify/{verToken}")
     public ResponseEntity<String> verify(@PathVariable(value = "verToken") String verificationToken, String username) {
         return authService.verifyAccount(verificationToken, username) ?
                 new ResponseEntity<>("User verification successful", HttpStatus.ACCEPTED):
                 new ResponseEntity<>("User verification unsuccessful", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping(value = "/login")
+    public ResponseBody login(@RequestBody LoginForm loginForm) {
+        return (ResponseBody) authService.loginUser(loginForm);
     }
 }
