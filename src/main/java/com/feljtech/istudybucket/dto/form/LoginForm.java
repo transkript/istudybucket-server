1a3d429dab4342cba637b4be698dcd0be0314a0e
elroykanye <elroykanye@gmail.com>
2021-09-18 18:02:15 +0100

    added jwt auth using jwt api: modified ./src/main/java/com/feljtech/istudybucket/dto/form/LoginForm.java

--- src/main/java/com/feljtech/istudybucket/dto/form/LoginForm.java
+++ src/main/java/com/feljtech/istudybucket/dto/form/LoginForm.java
@@ -1,2 +1,18 @@
-package com.feljtech.istudybucket.dto.form;public class LoginForm {
+package com.feljtech.istudybucket.dto.form;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginForm {
+    @JsonProperty(value = "username", required = true)
+    private String username;
+    @JsonProperty(value = "password", required = true)
+    private String password;
 }
