de4dcd53d7e1421dcf1b7be60cacb6a6494d9b0f
elroykanye <elroykanye@gmail.com>
2021-09-18 18:02:14 +0100

    added jwt auth using jwt api: modified ./src/main/java/com/feljtech/istudybucket/service/AuthService.java

--- src/main/java/com/feljtech/istudybucket/service/AuthService.java
+++ src/main/java/com/feljtech/istudybucket/service/AuthService.java
@@ -1,9 +1,14 @@
 package com.feljtech.istudybucket.service;
 
+import com.feljtech.istudybucket.dto.form.LoginForm;
 import com.feljtech.istudybucket.dto.form.RegisterForm;
+import com.feljtech.istudybucket.dto.response.AuthResponse;
 
 public interface AuthService {
     void registerAccount(RegisterForm registerForm);
 
     boolean verifyAccount(String verificationToken, String username);
+
+    AuthResponse loginUser(LoginForm loginForm);
+
 }
