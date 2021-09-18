e4de2ea9756b6ceeed59b78ea992669079a6cd9d
elroykanye <elroykanye@gmail.com>
2021-09-18 18:02:15 +0100

    added jwt auth using jwt api: modified ./src/main/java/com/feljtech/istudybucket/service/MailService.java

--- src/main/java/com/feljtech/istudybucket/service/MailService.java
+++ src/main/java/com/feljtech/istudybucket/service/MailService.java
@@ -1,9 +1,13 @@
 package com.feljtech.istudybucket.service;
 
 import com.feljtech.istudybucket.dto.email.VerificationEmail;
+import org.springframework.scheduling.annotation.Async;
 
 public interface MailService {
 
+    @Async
     void sendVerificationEmail(VerificationEmail verificationEmail);
+    // TODO refine all delaying methods using asynchronous programming
+    // TODO research on rabbitMQ
 
 }
