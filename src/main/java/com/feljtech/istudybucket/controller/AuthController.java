2a22b69c2c2c9ff176c3b609d397f175c5952fb9
elroykanye <elroykanye@gmail.com>
2021-09-19 18:29:57 +0100

    jwt security config done and tested: modified ./src/main/java/com/feljtech/istudybucket/controller/AuthController.java

--- src/main/java/com/feljtech/istudybucket/controller/AuthController.java
+++ src/main/java/com/feljtech/istudybucket/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.feljtech.istudybucket.dto.form.RegisterForm;
 import com.feljtech.istudybucket.service.AuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,7 +31,7 @@ public class AuthController {
      * @param registerForm: the form as an object - spring auto converts based on input names.
      * @return http status of the process
      */
-    @PostMapping(value = "/register")
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> register(@RequestBody RegisterForm registerForm) {
         authService.registerAccount(registerForm);
         return new ResponseEntity<>("User registration successful", HttpStatus.OK);
@@ -42,15 +43,21 @@ public class AuthController {
      * @param username: username of the associated user
      * @return response entity object describing success of operation
      */
-    @GetMapping(value = "/verify/{verToken}")
-    public ResponseEntity<String> verify(@PathVariable(value = "verToken") String verificationToken, String username) {
+    @GetMapping(value = "/verify/{username}")
+    public ResponseEntity<String> verify(@RequestParam(value = "verToken") String verificationToken, @PathVariable String username) {
         return authService.verifyAccount(verificationToken, username) ?
                 new ResponseEntity<>("User verification successful", HttpStatus.ACCEPTED):
                 new ResponseEntity<>("User verification unsuccessful", HttpStatus.BAD_REQUEST);
     }
 
-    @PostMapping(value = "/login")
-    public ResponseBody login(@RequestBody LoginForm loginForm) {
-        return (ResponseBody) authService.loginUser(loginForm);
+    /**
+     * activate user authentication process
+     * @param loginForm: request body of the endpoint
+     * @return: response containing jwtResponse body
+     * @throws Exception: in case auth failed
+     */
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> login(@RequestBody LoginForm loginForm) throws Exception {
+        return authService.loginUser(loginForm);
     }
 }
