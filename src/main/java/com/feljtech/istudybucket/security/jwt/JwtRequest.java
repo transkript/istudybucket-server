f82a797d6ae91de0b51adedde08550a986c6580c
elroykanye <elroykanye@gmail.com>
2021-09-19 18:29:55 +0100

    jwt security config done and tested: modified moasndon

---
+++ src/main/java/com/feljtech/istudybucket/security/jwt/JwtRequest.java
@@ -0,0 +1,2 @@
+package com.feljtech.istudybucket.security.jwt;public class JwtRequest {
+}
---
+++ src/main/java/com/feljtech/istudybucket/security/jwt/JwtResponse.java
@@ -0,0 +1,6 @@
+package com.feljtech.istudybucket.security.jwt;
+
+public class JwtResponse {
+    public JwtResponse(String token) {
+    }
+}
