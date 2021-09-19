ab4714fa11f22f8be5ca073e50860f2a1ed9db5d
elroykanye <elroykanye@gmail.com>
2021-09-19 18:29:56 +0100

    jwt security config done and tested: modified ./src/main/java/com/feljtech/istudybucket/service/impl/AuthServiceImpl.java

--- src/main/java/com/feljtech/istudybucket/service/impl/AuthServiceImpl.java
+++ src/main/java/com/feljtech/istudybucket/service/impl/AuthServiceImpl.java
@@ -3,20 +3,24 @@ package com.feljtech.istudybucket.service.impl;
 import com.feljtech.istudybucket.dto.email.VerificationEmail;
 import com.feljtech.istudybucket.dto.form.LoginForm;
 import com.feljtech.istudybucket.dto.form.RegisterForm;
-import com.feljtech.istudybucket.dto.response.AuthResponse;
 import com.feljtech.istudybucket.entity.User;
 import com.feljtech.istudybucket.entity.VerificationToken;
 import com.feljtech.istudybucket.enums.UserRole;
 import com.feljtech.istudybucket.repository.UserRepository;
 import com.feljtech.istudybucket.repository.VerificationTokenRepository;
+import com.feljtech.istudybucket.security.jwt.JwtResponse;
+import com.feljtech.istudybucket.security.jwt.JwtTokenUtil;
 import com.feljtech.istudybucket.service.AuthService;
 import com.feljtech.istudybucket.service.MailService;
-import com.feljtech.istudybucket.service.security.JwtProvider;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,19 +33,27 @@ import java.util.concurrent.atomic.AtomicBoolean;
 @SuppressWarnings("FieldCanBeLocal")
 @Service
 @AllArgsConstructor
+@Slf4j
 public class AuthServiceImpl implements AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
     private final MailService mailService;
     private final AuthenticationManager authenticationManager;
-    private final JwtProvider jwtProvider;
 
     private final String BASE_URL = "http://localhost:8080/";
+    private UserDetailsService userDetailsService;
+    private JwtTokenUtil jwtTokenUtil;
 
+    /**
+     * implementation for user registeration
+     * @param registerForm : request body for registration
+     * @return response entity based on success
+     */
     @Override
     @Transactional
-    public void registerAccount(RegisterForm registerForm) {
+    public ResponseEntity<?> registerAccount(RegisterForm registerForm) {
+        // TODO refactor this method for response entity
         User newUser = User.builder() // build the new User object from the register form
                 .username(registerForm.getUsername())
                 .email(registerForm.getEmail())
@@ -68,52 +80,78 @@ public class AuthServiceImpl implements AuthService {
 
         // send the email through MailService
         mailService.sendVerificationEmail(verificationEmail);
+
+        return new ResponseEntity<>("Registration successful", HttpStatus.OK);
     }
 
+    /**
+     * implementation for user verification
+     * @param verificationTokenValue: token value
+     * @param username: username concerned
+     * @return: logic state of user verification
+     */
     @Override
     @Transactional
     public boolean verifyAccount(String verificationTokenValue, String username) {
-        AtomicBoolean accountValid = new AtomicBoolean(false);
+        AtomicBoolean verTokenValid = new AtomicBoolean(false);
 
         // extract optional verification token from the db
         Optional<VerificationToken> verificationTokenOpt = verificationTokenRepository.findByTokenValue(verificationTokenValue);
 
         verificationTokenOpt.ifPresent(verificationToken -> {
             // set the account valid value based on the verification
-            accountValid.set(verificationToken.getUser().getUsername().equals(username));
+            verTokenValid.set(verificationToken.getUsername().equals(username));
 
             // get the Optional value for the user from the userRep object
-            Optional<User> userToBeVerified = userRepository.findById(verificationToken.getUser().getUserId());
+            Optional<User> userToBeVerified = userRepository.findByUsername(verificationToken.getUsername());
 
             // if user is present, perform the action declared,
             // pass call to update the user's verification
-            userToBeVerified.ifPresent(user -> updateUserVerification(user, accountValid.get()));
+            userToBeVerified.ifPresent(user -> {
+                boolean userUpdated = false;
+                if(verTokenValid.get()) {
+                    userUpdated = updateUserVerification(user, verTokenValid.get());
+                }
+                // delete the verification token if the operation was successful
 
-            // delete the verification token if the operation was successful
-            if (accountValid.get()) verificationTokenRepository.deleteById(verificationToken.getTokenId());
+                if(userUpdated) verificationTokenRepository.deleteById(verificationToken.getTokenId());
+            });
         });
-        return accountValid.get();
+        return verTokenValid.get();
     }
 
-    @Override
-    public AuthResponse loginUser(LoginForm loginForm) {
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        loginForm.getUsername(),
-                        loginForm.getPassword()
-                )
-        );
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwToken = jwtProvider.generateJwt(authentication);
-        return new AuthResponse(jwToken, loginForm.getUsername());
+    /**
+     * implementation for user authentication
+     * @param loginForm: request body for this method
+     * @return: response entity containing status and jwtResponse body
+     * @throws Exception: in case authentication is unsuccessful
+     */
+    public ResponseEntity<?> loginUser(LoginForm loginForm) throws Exception {
+
+        authenticateUser(loginForm);
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginForm.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return new ResponseEntity<>(new JwtResponse(token), HttpStatus.OK);
     }
 
-    // helper methods for this class
+    /* ************* helper methods for this class ************* */
+    private void authenticateUser(LoginForm loginForm) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginForm.getUsername(), loginForm.getPassword())
+            );
+        } catch (AuthenticationException e) {
+            throw new Exception("AUTH FAILED", e);
+        }
+    }
+
     private String generateVerificationToken(User newUser) {
         String token = UUID.randomUUID().toString();
         VerificationToken verificationToken = VerificationToken.builder() // build the verification token object
                 .tokenValue(token)
-                .user(newUser)
+                .username(newUser.getUsername())
+                .userEmail(newUser.getEmail())
                 .build();
         verificationTokenRepository.save(verificationToken); // save to the table
         return token;
@@ -123,9 +161,16 @@ public class AuthServiceImpl implements AuthService {
         return passwordEncoder.encode(password);
     }
 
-    private void updateUserVerification(User user, Boolean verified) {
-        userRepository.deleteById(user.getUserId()); // delete from the db by that user id
-        user.setUserVerified(verified); // update the value of user enabled
-        userRepository.save(user); // saved the updated user
+    private boolean updateUserVerification(User user, Boolean verified) {
+        boolean verSuccess = false;
+        try {
+            user.setUserVerified(verified); // update the value of user enabled
+            userRepository.save(user); // saved the updated user
+            verSuccess = true;
+        } catch (Exception e) {
+            log.error("User verification update failed");
+        }
+        return verSuccess;
     }
+    /* ************* helper methods for this class ************* */
 }
