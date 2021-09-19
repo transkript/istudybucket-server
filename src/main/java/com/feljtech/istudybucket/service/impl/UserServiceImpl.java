d5f04b2cf7c8a462a9931ed934b6d971eafe713c
elroykanye <elroykanye@gmail.com>
2021-09-19 18:29:56 +0100

    jwt security config done and tested: modified ./src/main/java/com/feljtech/istudybucket/service/impl/UserServiceImpl.java

--- src/main/java/com/feljtech/istudybucket/service/impl/UserServiceImpl.java
+++ src/main/java/com/feljtech/istudybucket/service/impl/UserServiceImpl.java
@@ -1,27 +1,21 @@
 package com.feljtech.istudybucket.service.impl;
 
 import com.feljtech.istudybucket.entity.User;
-import com.feljtech.istudybucket.dto.form.RegisterForm;
 import com.feljtech.istudybucket.repository.UserRepository;
 import com.feljtech.istudybucket.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
-    public Long registerUser(RegisterForm registerForm) {
-
-        User user = User.builder()
-                .username(registerForm.getUsername()).build();
-        return userRepository.save(user).getUserId();
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
