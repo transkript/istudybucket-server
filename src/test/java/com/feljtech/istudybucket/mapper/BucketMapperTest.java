3e8f4f1d40d431313148ad6a71e2b926edd101e9
elroykanye <elroykanye@gmail.com>
2021-09-25 23:55:53 +0100

    into mappers: modified ./src/test/java/com/feljtech/istudybucket/mapper/BucketMapperTest.java

--- src/test/java/com/feljtech/istudybucket/mapper/BucketMapperTest.java
+++ src/test/java/com/feljtech/istudybucket/mapper/BucketMapperTest.java
@@ -1,15 +1,34 @@
 package com.feljtech.istudybucket.mapper;
 
+import com.feljtech.istudybucket.dto.BucketDto;
+import com.feljtech.istudybucket.entity.Bucket;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.Instant;
+
+@SpringBootTest
 class BucketMapperTest {
+    private final BucketMapper bucketMapper;// = Mappers.getMapper(BucketMapper.class);
+
+    BucketMapperTest(BucketMapper bucketMapper) {
+        this.bucketMapper = bucketMapper;
+    }
+
 
     @Test
     void mapBucketToDto() {
+        Bucket bucket = Bucket.builder()
+                .bucketTitle("Test Bucket")
+                .description("My test bucket")
+                .creatorName("Elroy Kanye")
+                .creationDate(Instant.now())
+                .groupImage("groupImage")
+                .participants(3L).build();
+        BucketDto bucketDto = bucketMapper.mapBucketToDto(bucket);
+
+        //assertSame(bucketDto, bucket);
     }
 
-    @Test
-    void mapMemberships() {
-    }
+
 }
