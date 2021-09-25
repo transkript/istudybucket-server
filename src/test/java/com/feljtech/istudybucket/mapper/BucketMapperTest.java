8436c70671d8f1bb78b28eeebd4d163ad5f5d917
elroykanye <elroykanye@gmail.com>
2021-09-25 23:55:49 +0100

    into mappers: modified moasndon

---
+++ src/test/java/com/feljtech/istudybucket/mapper/BucketMapperTest.java
@@ -0,0 +1,15 @@
+package com.feljtech.istudybucket.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class BucketMapperTest {
+
+    @Test
+    void mapBucketToDto() {
+    }
+
+    @Test
+    void mapMemberships() {
+    }
+}
