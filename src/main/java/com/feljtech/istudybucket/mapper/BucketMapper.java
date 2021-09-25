364c8068ae911503fd3d783a4fa43ca00851ff77
elroykanye <elroykanye@gmail.com>
2021-09-25 23:55:52 +0100

    into mappers: modified ./src/main/java/com/feljtech/istudybucket/mapper/BucketMapper.java

--- src/main/java/com/feljtech/istudybucket/mapper/BucketMapper.java
+++ src/main/java/com/feljtech/istudybucket/mapper/BucketMapper.java
@@ -1,2 +1,24 @@
-package com.feljtech.istudybucket.mapper;public interface BucketMapper {
+package com.feljtech.istudybucket.mapper;
+
+import com.feljtech.istudybucket.dto.BucketDto;
+import com.feljtech.istudybucket.entity.Bucket;
+import com.feljtech.istudybucket.entity.relation.UserInBucket;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BucketMapper {
+
+    @Mapping(
+            target = "numberOfMembers",
+            expression = "java(mapMemberships(bucket.getMemberships()))"
+    )
+    BucketDto mapBucketToDto(Bucket bucket);
+
+    default Long mapMemberships(List<UserInBucket> memberships) {
+        return (long) memberships.size();
+    }
 }
