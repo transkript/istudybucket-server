62c4d603646cd90ee502c663bebb9ddbf5c43459
elroykanye <elroykanye@gmail.com>
2021-09-26 21:51:21 +0100

    work with post entity and layers: modified ./src/main/java/com/feljtech/istudybucket/dto/PostDto.java

--- src/main/java/com/feljtech/istudybucket/dto/PostDto.java
+++ src/main/java/com/feljtech/istudybucket/dto/PostDto.java
@@ -1,2 +1,37 @@
-package com.feljtech.istudybucket.dto;public class PostDto {
+package com.feljtech.istudybucket.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+@Builder
+public class PostDto {
+    @JsonProperty(value = "post_id")
+    private Long postId;
+
+    @JsonProperty(value = "post_title")
+    private String postTitle;
+
+    @JsonProperty(value = "content")
+    private String content;
+
+    @JsonProperty(value = "up_votes")
+    private Integer upVotes;
+
+    @JsonProperty(value = "down_votes")
+    private Integer downVotes;
+
+    @JsonProperty(value = "comment_count")
+    private Integer commentCount;
+
+    @JsonProperty(value = "post_type")
+    private String postType;
+
+    @JsonProperty(value = "author_name")
+    private String authorName;
 }
