809a32a8d33844ef718fbd681f53eeeb45377855
elroykanye <elroykanye@gmail.com>
2021-09-26 21:51:18 +0100

    work with post entity and layers: modified ./src/main/java/com/feljtech/istudybucket/repository/PostRepository.java

--- src/main/java/com/feljtech/istudybucket/repository/PostRepository.java
+++ src/main/java/com/feljtech/istudybucket/repository/PostRepository.java
@@ -1,2 +1,9 @@
-package com.feljtech.istudybucket.repository;public class PostRepository {
+package com.feljtech.istudybucket.repository;
+
+import com.feljtech.istudybucket.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
