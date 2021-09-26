54e9677b37b9de50c1dfa83a3a0d29d4430d5c94
elroykanye <elroykanye@gmail.com>
2021-09-26 21:51:19 +0100

    work with post entity and layers: modified ./src/main/java/com/feljtech/istudybucket/service/PostService.java

--- src/main/java/com/feljtech/istudybucket/service/PostService.java
+++ src/main/java/com/feljtech/istudybucket/service/PostService.java
@@ -1,2 +1,16 @@
-package com.feljtech.istudybucket.service;public interface PostService {
+package com.feljtech.istudybucket.service;
+
+import com.feljtech.istudybucket.dto.PostDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface PostService {
+    ResponseEntity<String> addPost(PostDto postDto);
+
+    List<PostDto> getAllPosts();
+
+    List<PostDto> getAllPostsByAuthorName(String authorName);
+
+    ResponseEntity<?> getPostById(Long postId);
 }
