9ee002e1e1ecdb1f0ce9c576612914fa8726889b
elroykanye <elroykanye@gmail.com>
2021-09-27 18:53:46 +0100

    votes d comments: modified ./src/main/java/com/feljtech/istudybucket/dto/VoteDto.java

--- src/main/java/com/feljtech/istudybucket/dto/VoteDto.java
+++ src/main/java/com/feljtech/istudybucket/dto/VoteDto.java
@@ -1,2 +1,14 @@
-package com.feljtech.istudybucket.dto;public class VoteDto {
+package com.feljtech.istudybucket.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class VoteDto {
+    @JsonProperty(value = "user_id")
+    private Long userId;
+
+    @JsonProperty(value = "post_id")
+    private Long postId;
+
+    @JsonProperty(value = "vote")
+    private Integer vote;
 }
