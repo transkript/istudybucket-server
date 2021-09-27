c35ab4a32da318d1c8427b8341ecbff0224d07d1
elroykanye <elroykanye@gmail.com>
2021-09-27 18:53:36 +0100

    votes d comments: modified ./src/main/java/com/feljtech/istudybucket/util/InstantUtil.java

--- src/main/java/com/feljtech/istudybucket/util/InstantUtil.java
+++ src/main/java/com/feljtech/istudybucket/util/InstantUtil.java
@@ -1,2 +1,17 @@
-package com.feljtech.istudybucket.util;public class InstantUtil {
+package com.feljtech.istudybucket.util;
+
+import java.time.Instant;
+
+public class InstantUtil {
+    public static String extractDateFromInstant(Instant instant) {
+        return splitInstant(instant)[0];
+    }
+
+    public static String extractTimeFromInstant(Instant instant) {
+        return splitInstant(instant)[1];
+    }
+
+    private static String[] splitInstant(Instant instant) {
+        return instant.toString().split("T");
+    }
 }
