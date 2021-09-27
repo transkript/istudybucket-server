aacd3e87fcac75f2a4685dcaf18d2cd4619d12a6
elroykanye <elroykanye@gmail.com>
2021-09-27 18:53:39 +0100

    votes d comments: modified ./src/main/java/com/feljtech/istudybucket/service/CommentService.java

--- src/main/java/com/feljtech/istudybucket/service/CommentService.java
+++ src/main/java/com/feljtech/istudybucket/service/CommentService.java
@@ -1,2 +1,14 @@
-package com.feljtech.istudybucket.service;public class CommentService {
+package com.feljtech.istudybucket.service;
+
+import com.feljtech.istudybucket.dto.CommentDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface CommentService {
+    ResponseEntity<String> addComment(CommentDto commentDto);
+
+    ResponseEntity<List<CommentDto>> getCommentsByPost(Long postId);
+
+    ResponseEntity<List<CommentDto>> getCommentsByPostAndAuthor(Long postId, Long authorId);
 }
