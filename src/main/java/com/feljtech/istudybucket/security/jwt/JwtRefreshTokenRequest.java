9bfcb4fb08344229428fda861a50f8dc0a2c67c0
elroykanye <elroykanye@gmail.com>
2021-10-01 19:48:22 +0100

    added logout and token refresh func: modified ./src/main/java/com/feljtech/istudybucket/security/jwt/JwtRefreshTokenRequest.java

--- src/main/java/com/feljtech/istudybucket/security/jwt/JwtRefreshTokenRequest.java
+++ src/main/java/com/feljtech/istudybucket/security/jwt/JwtRefreshTokenRequest.java
@@ -1,2 +1,17 @@
-package com.feljtech.istudybucket.security.jwt;public class JwtRefreshTokenRequest {
+package com.feljtech.istudybucket.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtRefreshTokenRequest {
+    @NotBlank
+    private String refreshToken;
+    private String username;
+
 }
