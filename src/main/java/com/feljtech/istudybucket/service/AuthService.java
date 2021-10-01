55b32ab2c06b1dd1ae960145e969fa6c9ce7ade1
elroykanye <elroykanye@gmail.com>
2021-10-01 19:48:22 +0100

    added logout and token refresh func: modified ./src/main/java/com/feljtech/istudybucket/service/AuthService.java

--- src/main/java/com/feljtech/istudybucket/service/AuthService.java
+++ src/main/java/com/feljtech/istudybucket/service/AuthService.java
@@ -1,13 +1,20 @@
 package com.feljtech.istudybucket.service;
 
-import com.feljtech.istudybucket.dto.request.UserLoginRequest;
-import com.feljtech.istudybucket.dto.request.UserRegisterRequest;
+import com.feljtech.istudybucket.dto.request.LoginRequest;
+import com.feljtech.istudybucket.dto.request.RegisterRequest;
+import com.feljtech.istudybucket.security.jwt.JwtRefreshTokenRequest;
 import org.springframework.http.ResponseEntity;
 
 public interface AuthService {
-    ResponseEntity<?> registerAccount(UserRegisterRequest userRegisterRequest);
+    ResponseEntity<?> registerAccount(RegisterRequest registerRequest);
 
     boolean verifyAccount(String verificationToken, String username);
 
-    ResponseEntity<?> loginUser(UserLoginRequest userLoginRequest) throws Exception;
+    ResponseEntity<?> loginUser(LoginRequest loginRequest) throws Exception;
+
+    ResponseEntity<?> validateRefreshToken(JwtRefreshTokenRequest jwtRefreshTokenRequest);
+
+    ResponseEntity<?> refreshToken(JwtRefreshTokenRequest jwtRefreshTokenRequest);
+
+    ResponseEntity<String> logoutUser(JwtRefreshTokenRequest jwtRefreshTokenRequest);
 }
