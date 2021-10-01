3b0ed18688f092133edd120f22116caead3690b7
elroykanye <elroykanye@gmail.com>
2021-10-01 19:48:24 +0100

    added logout and token refresh func: modified ./src/main/java/com/feljtech/istudybucket/entity/composite/UserLinkUserKey.java

--- src/main/java/com/feljtech/istudybucket/entity/composite/UserLinkUserKey.java
+++ src/main/java/com/feljtech/istudybucket/entity/composite/UserLinkUserKey.java
@@ -1,2 +1,22 @@
-package com.feljtech.istudybucket.entity.composite;public class UserLinkUser {
+package com.feljtech.istudybucket.entity.composite;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class UserLinkUserKey implements Serializable {
+    @Column(name = "left_link")
+    private Long leftLinkUserId;
+
+    @Column(name = "right_link")
+    private Long rightLinkUserId;
 }
