f55a1285af4ee6d194607ef8e98d04406789691c
elroykanye <elroykanye@gmail.com>
2021-10-01 19:48:20 +0100

    added logout and token refresh func: modified ./src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java

--- src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java
+++ src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java
@@ -1,2 +1,14 @@
-package com.feljtech.istudybucket.repository;public class RefreshTokenRepository {
+package com.feljtech.istudybucket.repository;
+
+import com.feljtech.istudybucket.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByToken(String token);
 }
