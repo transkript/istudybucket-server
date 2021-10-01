548c6b2f66ab47b9bfede8a1cc1192da6055833f
elroykanye <elroykanye@gmail.com>
2021-10-01 19:48:24 +0100

    added logout and token refresh func: modified ./src/main/java/com/feljtech/istudybucket/entity/relation/UserLinkUser.java

--- src/main/java/com/feljtech/istudybucket/entity/relation/UserLinkUser.java
+++ src/main/java/com/feljtech/istudybucket/entity/relation/UserLinkUser.java
@@ -1,2 +1,39 @@
-package com.feljtech.istudybucket.entity.relation;public class UserLinkUser {
+package com.feljtech.istudybucket.entity.relation;
+
+import com.feljtech.istudybucket.entity.User;
+import com.feljtech.istudybucket.entity.composite.UserLinkUserKey;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class UserLinkUser {
+    @EmbeddedId
+    private UserLinkUserKey id;
+
+    @Column(name = "link_date")
+    private Instant linkDate;
+/*
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "right_link_user_user_id", nullable = false)
+    private User rightLinkUser;
+
+
+ */
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @MapsId("rightLinkUserId")
+    private User rightLinkUser;
+
+    @ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @MapsId("leftLinkUserId")
+    private User leftLinkUser;
+
 }
