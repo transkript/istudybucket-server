3c58b13abe9e60ac7fb160bc1a72833daf6977fc
elroykanye <elroykanye@gmail.com>
2021-10-02 15:53:35 +0100

    added swagger api doc: modified ./src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java

--- src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java
+++ src/main/java/com/feljtech/istudybucket/repository/RefreshTokenRepository.java
@@ -8,7 +8,8 @@ import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByRefreshToken(String token);
 
-    void deleteByToken(String token);
+    void deleteByRefreshToken(String token);
+
 }
