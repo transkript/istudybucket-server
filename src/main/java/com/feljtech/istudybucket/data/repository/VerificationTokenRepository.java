222548c76174307407f75c2505d3c1b4910772d6
elroykanye <elroykanye@gmail.com>
2021-10-04 17:11:10 +0100

    refactored package arrangement to abcd: modified ./src/main/java/com/feljtech/istudybucket/data/repository/VerificationTokenRepository.java

--- src/main/java/com/feljtech/istudybucket/data/repository/VerificationTokenRepository.java
+++ src/main/java/com/feljtech/istudybucket/data/repository/VerificationTokenRepository.java
@@ -1,9 +1,8 @@
-package com.feljtech.istudybucket.repository;
+package com.feljtech.istudybucket.data.repository;
 
-import com.feljtech.istudybucket.entity.VerificationToken;
+import com.feljtech.istudybucket.data.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
