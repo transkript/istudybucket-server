b5dca0eb3fc96e045d12553239a8a4d87536cbc7
elroykanye <elroykanye@gmail.com>
2021-10-04 17:11:10 +0100

    refactored package arrangement to abcd: modified ./src/main/java/com/feljtech/istudybucket/data/repository/BucketRepository.java

--- src/main/java/com/feljtech/istudybucket/data/repository/BucketRepository.java
+++ src/main/java/com/feljtech/istudybucket/data/repository/BucketRepository.java
@@ -1,6 +1,6 @@
-package com.feljtech.istudybucket.repository;
+package com.feljtech.istudybucket.data.repository;
 
-import com.feljtech.istudybucket.entity.Bucket;
+import com.feljtech.istudybucket.data.entity.Bucket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BucketRepository extends JpaRepository<Bucket, Long> {
