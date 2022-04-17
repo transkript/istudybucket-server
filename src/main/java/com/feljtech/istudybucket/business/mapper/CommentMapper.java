1ba97583c78ac705cea22a417041ca3c130bc8b8
Elroy Kanye <elroykanye@gmail.com>
2022-04-17 16:52:28 +0100

    add tests for the CommentMapper

--- src/main/java/com/feljtech/istudybucket/business/mapper/CommentMapper.java
+++ src/main/java/com/feljtech/istudybucket/business/mapper/CommentMapper.java
@@ -1,13 +1,12 @@
 package com.feljtech.istudybucket.business.mapper;
 
 import com.feljtech.istudybucket.api.dto.CommentDto;
-import com.feljtech.istudybucket.data.entity.Comment;
-import com.feljtech.istudybucket.data.entity.Post;
-import com.feljtech.istudybucket.data.entity.User;
 import com.feljtech.istudybucket.business.util.InstantUtil;
+import com.feljtech.istudybucket.data.entity.Comment;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 import java.time.Instant;
 
@@ -16,27 +15,25 @@ import java.time.Instant;
  */
 @Mapper(componentModel = "spring")
 public interface CommentMapper {
-    @Mapping(target = "postId", expression = "java(mapPostId(comment.getSourcePost()))")
-    @Mapping(target = "authorName", expression = "java(mapAuthorName(comment.getAuthor()))")
-    @Mapping(target = "creationDate", expression = "java(mapCreationDate(comment.getCreationDate()))")
+    @Mappings({
+            @Mapping(target = "postId", expression = "java(comment.getSourcePost().getPostId())"),
+            @Mapping(target = "creationDate", expression = "java(mapCreationDate(comment.getCreationDate()))"),
+            @Mapping(target = "authorId", expression = "java(comment.getAuthor().getUserId())"),
+    })
     CommentDto mapCommentToDto(Comment comment);
 
-    @InheritInverseConfiguration
-    @Mapping(target = "author", ignore = true)
-    @Mapping(target = "sourcePost", ignore = true)
-    @Mapping(target = "creationDate", expression = "java(inverseMapCreationDate())")
-    Comment mapDtoToComment(CommentDto commentDto);
-
     // map helpers
-    default Long mapPostId(Post sourcePost) {
-        return sourcePost.getPostId();
-    }
-    default String mapAuthorName(User author) {
-        return author.getUsername();
-    }
     default String mapCreationDate(Instant creationDate) {
         return InstantUtil.extractDateFromInstant(creationDate);
     }
+
+    @InheritInverseConfiguration
+    @Mappings({
+            @Mapping(target = "author", ignore = true),
+            @Mapping(target = "sourcePost", ignore = true),
+            @Mapping(target = "creationDate", expression = "java(inverseMapCreationDate())"),
+    })
+    Comment mapDtoToComment(CommentDto commentDto);
 
     // inverse map helpers
     default Instant inverseMapCreationDate() {
