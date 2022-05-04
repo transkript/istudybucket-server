2fbf41b180779235ee648ff76e10112d8cd7f195
Elroy Kanye <elroykanye@gmail.com>
2022-05-04 01:40:05 +0100

    add constructors for PostDto

--- isb-library/src/main/java/com/elroykanye/istudybucket/api/dto/PostDto.java
+++ isb-library/src/main/java/com/elroykanye/istudybucket/api/dto/PostDto.java
@@ -1,15 +1,17 @@
 package com.elroykanye.istudybucket.api.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDto {
     @JsonProperty(value = "post_id")
     private Long postId;
