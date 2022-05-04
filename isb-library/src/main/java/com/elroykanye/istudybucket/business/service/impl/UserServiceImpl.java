03b331ba7918674325c420607505f0056122fbbe
Elroy Kanye <elroykanye@gmail.com>
2022-05-04 02:26:31 +0100

    add new methods from interface

--- isb-library/src/main/java/com/elroykanye/istudybucket/business/service/impl/UserServiceImpl.java
+++ isb-library/src/main/java/com/elroykanye/istudybucket/business/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.elroykanye.istudybucket.business.service.impl;
 
 import com.elroykanye.istudybucket.api.dto.UserDto;
+import com.elroykanye.istudybucket.business.mapper.UserMapper;
 import com.elroykanye.istudybucket.business.service.UserService;
 import com.elroykanye.istudybucket.data.entity.User;
 import com.elroykanye.istudybucket.data.enums.UserRole;
@@ -13,12 +14,14 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
 
     @Override
@@ -49,17 +52,6 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUser(Long id) {
-        log.info("Getting user with id: {}", id);
-        Optional<User> userOptional = userRepository.findById(id);
-        if(userOptional.isPresent()) {
-            return userOptional.get();
-        } else {
-            throw new EntityException.EntityNotFoundException("user", id);
-        }
-    }
-
-    @Override
     public User getUser(String username) {
         log.info("Getting user with username: {}", username);
         Optional<User> userOptional = userRepository.findByUsername(username);
@@ -71,8 +63,57 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public List<User> getAllUsers() {
+    public UserDto getUserById(Long id) {
+        log.info("Getting user with id: {}", id);
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()) {
+            log.info("User found");
+            return userMapper.mapUserToDto(userOptional.get());
+        }
+        log.warn("User not found");
+        throw new EntityException.EntityNotFoundException("user", id);
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
         log.info("Getting all users");
-        return userRepository.findAll();
+        return userRepository.findAll().stream().map(userMapper::mapUserToDto).collect(Collectors.toList());
+    }
+
+    @Override
+    public UserDto getUserByUsername(String username) {
+        log.info("Getting user with username: {}", username);
+        Optional<User> userOptional = userRepository.findByUsername(username);
+        if(userOptional.isPresent()) {
+            log.info("User found");
+            return userMapper.mapUserToDto(userOptional.get());
+        }
+        log.warn("User not found");
+        throw new EntityException.EntityNotFoundException("user", username);
+    }
+
+    @Override
+    public String updateUser(UserDto userDto) {
+        log.info("Updating user with username: {}", userDto.getUsername());
+        Optional<User> userOptional = userRepository.findByUsername(userDto.getUsername());
+        if(userOptional.isPresent()) {
+            log.info("User found");
+            User userUpdated = userMapper.mapDtoToUser(userDto);
+
+            User user = userOptional.get();
+            user.setUsername(userUpdated.getUsername());
+            user.setEmail(userUpdated.getEmail());
+            user.setFirstName(userUpdated.getFirstName());
+            user.setLastName(userUpdated.getLastName());
+            user.setPhoneNumber(userUpdated.getPhoneNumber());
+            user.setDob(userUpdated.getDob());
+            user.setGender(userUpdated.getGender());
+
+            userRepository.save(user);
+
+            return "User updated successfully";
+        }
+        log.warn("User not found");
+        throw new EntityException.EntityNotFoundException("user", userDto.getUsername());
     }
 }
