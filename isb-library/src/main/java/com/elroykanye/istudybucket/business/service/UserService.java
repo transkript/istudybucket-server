2055e8e62abb5b91a7817623f6ac5eec7101c84a
Elroy Kanye <elroykanye@gmail.com>
2022-05-04 11:02:11 +0100

    intro methods for loading entities only

--- isb-library/src/main/java/com/elroykanye/istudybucket/business/service/UserService.java
+++ isb-library/src/main/java/com/elroykanye/istudybucket/business/service/UserService.java
@@ -11,6 +11,7 @@ import java.util.List;
 public interface UserService {
     Long saveUser(UserDto userDto);
 
+    User getUser(Long id);
     User getUser(String username);
 
     UserDto getUserById(Long id);
