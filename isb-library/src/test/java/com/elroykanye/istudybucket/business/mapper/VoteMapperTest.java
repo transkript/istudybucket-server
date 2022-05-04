56643235831fd93ef62853388115a08da370c101
Elroy Kanye <elroykanye@gmail.com>
2022-05-05 00:33:55 +0100

    added vote mapper

---
+++ isb-library/src/test/java/com/elroykanye/istudybucket/business/mapper/VoteMapperTest.java
@@ -0,0 +1,14 @@
+package com.elroykanye.istudybucket.business.mapper;
+
+import org.junit.jupiter.api.Test;
+
+class VoteMapperTest {
+
+    @Test
+    void mapVoteToDto() {
+    }
+
+    @Test
+    void mapDtoToVote() {
+    }
+}
