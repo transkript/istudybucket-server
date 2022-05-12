1d2ae3298a4e62531d0baf6197cc37a33812e223
Elroy Kanye <elroykanye@gmail.com>
2022-05-12 19:59:22 +0100

    adjust comment beans to use post instead

--- isb-library/src/main/java/com/elroykanye/istudybucket/IstudybucketApplication.java
+++ isb-library/src/main/java/com/elroykanye/istudybucket/IstudybucketApplication.java
@@ -3,7 +3,6 @@ package com.elroykanye.istudybucket;
 import com.elroykanye.istudybucket.config.global.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
