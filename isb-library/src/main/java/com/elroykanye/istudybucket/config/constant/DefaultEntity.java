3ad6c7f119a7942ba31440a11bb0403d7fff8bc6
Tanju Brunostar <tanjubrunostar0@gmail.com>
2022-05-12 23:11:55 +0100

    changed creation date type from instance to LocalDateTime in Default entity and Dto

--- isb-library/src/main/java/com/elroykanye/istudybucket/config/constant/DefaultEntity.java
+++ isb-library/src/main/java/com/elroykanye/istudybucket/config/constant/DefaultEntity.java
@@ -9,31 +9,32 @@ import com.elroykanye.istudybucket.data.enums.PostType;
 import com.elroykanye.istudybucket.data.enums.UserRole;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 public class DefaultEntity {
     public final static Bucket DEFAULT_BUCKET = Bucket.builder()
             .bucketId(1L).bucketTitle("Default Bucket").description("This is the default bucket")
-            .groupImage("/path/to/image").creationDate(Instant.now()).memberships(List.of())
+            .groupImage("/path/to/image").creationDate(LocalDateTime.now()).memberships(List.of())
             .build();
 
     public final static User DEFAULT_USER = User.builder()
             .userId(1L).username("user").password("password").email("user@mail.com")
             .firstName("First").lastName("Last").phoneNumber("1234567890")
-            .dob(new Date()).gender(Gender.MALE).userRole(UserRole.USER).userVerified(true)
-            .createdDate(Instant.now()).buckets(List.of()).messages(List.of())
+            .dob(LocalDateTime.now()).gender(Gender.MALE).userRole(UserRole.USER).userVerified(true)
+            .createdDate(LocalDateTime.now()).buckets(List.of()).messages(List.of())
             .trophies(List.of()).linkOfUsers(List.of()).linkToUsers(List.of()).buckets(List.of())
             .build();
 
     public final static Chat DEFAULT_CHAT = Chat.builder()
             .chatId(1L).chatTitle("Default Chat").chatType("GROUP")
-            .chatDescription("This is the default chat").creationDate(Instant.now())
+            .chatDescription("This is the default chat").creationDate(LocalDateTime.now())
             .messages(List.of())
             .build();
 
     public final static Post DEFAULT_POST = Post.builder()
-            .postId(1L).createdDate(Instant.now()).content("This is the default post")
+            .postId(1L).createdDate(LocalDateTime.now()).content("This is the default post")
             .postTitle("Default Post").postType(PostType.QUESTION)
             .votes(List.of())
             .comments(List.of())
